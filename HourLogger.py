38bd346faeaa5c72ab61195e1729cbbf82403aa1
nhodges78 <nhodges78@gmail.com>
2020-12-30 19:57:52 -0500

    skeleton complete

--- HourLogger.py
+++ HourLogger.py
@@ -1,13 +1,54 @@
 #hour logger program
 
-import calendar
+import calendar #for formatting calendar outputs on larger scale
+import datetime #for working with time values
+import os #for cleaning terminal
 
-myCalendar = calendar.Calendar(firstweekday=0) #monday is first day of week
+#FUNCTIONS
 
-#Option 1: Add Time
+def showMenu(): #display program options and get user input
+    print("       Hour Logger v1.0")
+    print("-------------------------------")
+    print("Please select an option below:")
+    print("[1] Add Time")
+    print("[2] Remove Time")
+    print("[3] Display Current Week's Time")
+    print("[4] Export Current Week's Time\n")
+    sel = input("=> ")
+    if sel<'1' or sel>'4': #make sure input is in bounds
+        while sel<'1' or sel>'4':
+            sel = input("Invalid input, select 1, 2, 3, or 4 => ")
 
+    return sel
 
+def quit(): #ask if user wants to keep using the program
+    print("\nContinue?\t[0] YES [1] NO")
+    sel = input("=> ")
+    if sel<'0' or sel>'1':
+        while sel<'0' or sel>'1':
+            sel = input("Invalid input, select 0 or 1 => ")
 
-#Option 2: Remove Time
-#Option 3: Display current calendar week's time
-#Option 4: Export current calendar week to txt
+    return sel
+
+#MAIN
+
+quitProgram = '0' #start while loop
+
+while quitProgram=='0':
+    select = showMenu()
+    
+    if select=='1': #Option 1: Add Time
+        print("added time")
+
+    if select=='2': #Option 2: Remove Time
+        print("removed time")
+
+    if select=='3': #Option 3: Display current calendar period's time
+        print("displaying")
+
+    if select=='4': #Option 4: Export current period to txt
+        print("exporting")
+
+    quitProgram = quit()
+
+print("\n\nDone.")
--- readme.txt
+++ readme.txt
@@ -2,7 +2,26 @@
 
 Goal: Program that tracks weekly time worked in hours. User can specify if they would like to look at the current calendar week to see time worked by day, whether they want to add or delete time, or if they'd like to export the current calendar week's time.
 
+System Setup - functions
+	
+	First, set menu that displays all options (use a function called showMenu)
+		
+		WILL NEED METHOD TO INPUT TEXT CALENDAR 
+	
+		Prompt for user to select an option and begin the appropriate process
+		
+			If input is not one of the four options, prompt for another input
+	Second, provide method for user to quit the program or continue using it
+
+MAIN Program Setup
+
+	First, need a while loop to keep the program looping as long as the user wants. This starts with quitProgram initializing to 0 and continues as long as it remains 0, checked at the end of every loop by quit().
+
+	Second, need an if statement to check the user input and perform the desired operation accordingly. These options are as follows:
+
 Option 1: Add Time
+
+	First, ask user to input time (may include option to use current system time later)
 
 Option 2: Remove Time
 
